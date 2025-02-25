493f45289433dcbb5504a6e5d5fcc046f2a21fb9
didsterio <smdidenko@gmail.com>
2025-02-26 00:17:12 +0300

    task 4 - 5
    completed

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -2,10 +2,27 @@ function highlight(table) {
   let tableRows = table.querySelectorAll('tr');
 
   tableRows.forEach(rowElement => {
-    if (rowElement.cells[3].dataset.available == "true") rowElement.classList.add('available'); else rowElement.classList.add('unavailable');
-    if (rowElement.cells[3].dataset.available == undefined) rowElement.hidden = true;
-    if (rowElement.cells[2].textContent == "m") rowElement.classList.add('male'); else rowElement.classList.add('female');
-    if (rowElement.cells[1].textContent < 18) rowElement.style.textDecoration = 'line-through';
+
+    if (rowElement.cells[3].dataset.available == "true") {
+      rowElement.classList.add('available');
+    } else {
+      rowElement.classList.add('unavailable');
+    }
+
+    if (rowElement.cells[3].dataset.available == undefined) {
+      rowElement.hidden = true;
+    }
+
+    if (rowElement.cells[2].textContent == "m") {
+      rowElement.classList.add('male');
+    }  else {
+      rowElement.classList.add('female');
+    }
+
+    if (rowElement.cells[1].textContent < 18) {
+      rowElement.style.textDecoration = 'line-through';
+    }
+    
   });
 
   return null;
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,24 @@
 function initCarousel() {
-  // ваш код...
+  let slider = document.querySelector('.carousel__inner');
+  let btnLeft = document.querySelector('.carousel__arrow_left');
+  let btnRight = document.querySelector('.carousel__arrow_right');
+  let slidesCount = document.querySelectorAll('.carousel__slide').length;
+  currentStep = 0;
+  sliderButtonsProcessing();
+
+  btnRight.addEventListener('click', () => {
+    currentStep -= 1;
+    sliderButtonsProcessing();
+  });
+
+  btnLeft.addEventListener('click', () => {
+    currentStep += 1;
+    sliderButtonsProcessing();
+  });
+  
+  function sliderButtonsProcessing() {
+    slider.style.transform = `translateX(${currentStep*slider.offsetWidth}px)`;
+    btnLeft.style.display = currentStep >= 0 ? 'none' : '';
+    btnRight.style.display = currentStep <= -(slidesCount - 1) ? 'none' : '';
+  }
 }
